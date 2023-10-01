b6b579da40027f7df33463bd8870481c5491531b
Sarthak Singh Tomar <singhtomarsarthak@gmail.com>
2023-10-02 00:54:15 +0530

    project intial setup

---
+++ src/test/java/com/backend/vehicle/api/search/VehicleSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.vehicle.api.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
